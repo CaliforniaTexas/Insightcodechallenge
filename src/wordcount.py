256623daf0fb0513802dba3c6b72fe4e850cfd0a
CaliforniaTexas <vemani9@gmail.com>
2015-05-11 15:38:12 -0700

    wcount

--- src/wordcount.py
+++ src/wordcount.py
@@ -1,3 +1,4 @@
+
 import re,os
 import string
 file=open("C:\\Users\\SIVA\\Desktop\\Code\\wc_input\\Wordcount.txt","r+").read()
@@ -10,15 +11,9 @@ for word in file.lower().split():
             word_count[word]+=1
 
 for item in word_count.items():
-    print ("{}\t{}".format(*item))
-
-f=open("C:\\Users\\SIVA\\Desktop\\Code\\wc_output\\wc_result.txt","w")
-for key,value in sorted(word_count.items()):
-    f.write(key)
-    f.write("\t") 
-    f.write(str(value))
-    f.write("\n") 
-
+    #print ("{}\t{}".format(*item))
+ f = open("C:\\Users\\SIVA\\Desktop\\Code\\wc_output\\wc_result.txt","w")
 for item in sorted(word_count.items()):
-    f.write("{}\t{}\n".format(*item))    
+    f.write("{}\t{}\n".format(*item))
+      
 f.close()
